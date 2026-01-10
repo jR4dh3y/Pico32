ca8862390995af8757b8a2a6c1ed425b49819818
Radhey Kalra <radheykalra901@gmail.com>
2026-01-10 14:09:00 +0530

    Add WiFiAttacks class with method declarations

--- src/WiFiAttacks.h
+++ src/WiFiAttacks.h
@@ -51,3 +51,64 @@ enum class WiFiMode : uint8_t {
     ATTACK_RICKROLL,
     ATTACK_FUNNY
 };
+
+class WiFiAttacks {
+public:
+    void begin();
+    void update();
+    void stop();
+    
+    // Scanning
+    void startScanAP();
+    void startScanStation();
+    void startSniffBeacon();
+    void startSniffProbe();
+    void startSniffDeauth();
+    void startSniffPMKID();
+    void startSniffPwn();
+    void startSniffRaw();
+    
+    // Attacks
+    void startDeauth();
+    void startBeaconRandom();
+    void startBeaconList();
+    void startRickRoll();
+    void startFunnyBeacon();
+    
+    // Channel
+    void setChannel(uint8_t channel);
+    uint8_t getChannel() const { return _channel; }
+    
+    // Target management
+    LinkedList<AccessPoint>* getAPs() { return &_accessPoints; }
+    LinkedList<Station>* getStations() { return &_stations; }
+    LinkedList<SSID>* getSSIDs() { return &_ssids; }
+    
+    void selectAP(uint8_t index, bool selected);
+    void addSSID(const char* ssid);
+    void clearAll();
+    
+    bool isActive() const { return _mode != WiFiMode::IDLE; }
+    
+    // Public for callback access
+    WiFiMode _mode = WiFiMode::IDLE;
+    uint32_t _packetCount = 0;
+    
+private:
+    uint8_t _channel = DEFAULT_CHANNEL;
+    uint32_t _lastUpdate = 0;
+    
+    LinkedList<AccessPoint> _accessPoints;
+    LinkedList<Station> _stations;
+    LinkedList<SSID> _ssids;
+    
+    // Internal methods
+    void sendDeauthToAll();
+    void sendDeauthFrame(const uint8_t* ap, const uint8_t* client, uint8_t channel);
+    void sendRandomBeacon();
+    void sendListBeacon();
+    void sendBeaconFrame(const char* ssid, uint8_t channel);
+    String macToString(const uint8_t* mac);
+};
+
+extern WiFiAttacks wifiAttacks;
